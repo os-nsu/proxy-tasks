34545b3d5caaef7a2f7b245c959da8385b4eda82
Grigoriy <reVInotip>
2025-01-17 13:36:09 +0700

    Create base interface for lab3: Proccesses (without ptrace)

--- src/include/master.h
+++ src/include/master.h
@@ -11,6 +11,30 @@
         src/include/master.h
 */
 
+//=========START_HOOK INTERFACE=========
+
+#include <stdint.h>
 typedef void (*Hook)(void);
 
 extern Hook executor_start_hook; //< is called immediately before start main loop of master process
+
+//=========REGISTER_WORKER_HOOK INTERFACE=========
+
+/*!
+    Defines process termination behavior
+*/
+typedef enum RestartConstants {
+    NEVER = 0, // Never restart worker's proccess after it termination
+    ALWAYS = 1 // Always restart worker's proccess after it termination
+} RestartConstants;
+
+/*!
+    Create new process for plugin
+    \param [in] name name of this proccess
+    \param [in] lib_name name of library (plugin) without .so
+    \param [in] func_name proccess's entrypoint
+    \param [in] restart defines process termination behavior
+    \param [in] cooldown time in seconds after which the process will be restarted.
+        If restart == NEVER this argument will be ignored.
+*/
+void register_worker(const char *name, const char *lib_name, const char *func_name, RestartConstants restart, uint32_t cooldown);
